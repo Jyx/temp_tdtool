3ac93f748cd05b28055e1dbc2d235a9c90338cda
Joakim Bech <joakim.bech@gmail.com>
2012-12-13 22:44:43 +0100

    Now use perl hash for sensors instead
    Signed-off-by: Joakim Bech <joakim.bech@gmail.com>

--- populate_rrd.pl
+++ populate_rrd.pl
@@ -1,52 +1,62 @@
 #!/usr/bin/perl
+################################################################################
+# Author: Joakim Bech (joakim.bech@gmail.com)                                  #
+# Site  : http://joakimbech.com                                                #
+#         http://jyx.se                                                        #
+#         http://jyx.dyndns.org                                                #
+################################################################################
 use strict;
 use warnings;
 use Switch;
 use RRDs;
 
-# The ID's of the sensors that you are interested in.
+# The ID's of the sensors that you are interested in. If you add remove these,
+# you also need to add and remove on all places below.
 my $DOWN = 132;
 my $UPPER = 123;
-my $OUT = 186;
+my $OUT = 10;
+
+my %temps = ( $DOWN, "NaN",
+	      $UPPER, "NaN",
+	      $OUT, "NaN" );
 
 # The full path to the rrd-database.
-#my $RRD_DB = "/home/jyx/bin/temp.rrd";
-my $RRD_DB = "/home/jyx/devel/temp_tdtool/temp.rrd";
+my $RRD_DB = "/home/jyx/bin/temp.rrd";
+#my $RRD_DB = "/home/jyx/devel/temp_tdtool/temp.rrd";
 my @tdtool_data = `tdtool -l`;
-
-my $temp_out;
-my $temp_upper;
-my $temp_down;
 
 foreach my $line (@tdtool_data) {
 	chomp($line);
 	$line =~ m/.*temperature[ \t]*(\d{1,3})[ \t]*([-\.0-9]*).*/;
 	my $sensor = $1 if defined $1;
 	my $temp = $2 if defined $2;
-	# If sendor or temp isn't set, then the match has probably found an
+	# If sensor or temp isn't set, then the match has probably found an
 	# invalid line from tdtool (humidity for example).
 	next if (not defined $sensor and not defined $temp);
 
 	switch ($sensor) {
-		case ($UPPER) { $temp_upper = $temp; }
-		case ($DOWN) { $temp_down = $temp; }
-		case ($OUT) { $temp_out = $temp; }
+		case ($UPPER) { $temps{$UPPER} = $temp; }
+		case ($DOWN) { $temps{$DOWN} = $temp; }
+		case ($OUT) { $temps{$OUT} = $temp; }
 	}
 
-	if (defined $temp_upper and $temp_down and $temp_out) {
-		my $time = `date +%s`;
+	# When all sensors has a valid value, we update the rrd database, and
+	# then immediately exit since we are done with out job.
+	if ($temps{$UPPER} ne "NaN" and
+	    $temps{$DOWN} ne "NaN" and
+	    $temps{$OUT} ne "NaN") {
+		my $time;
+		$time = `date +%s`; # Seconds since 1970-01-01 00:00:00 UTC
 		chomp($time);
 		# The order here is VERY important, it must match the same as
 		# the rrd-database.
-		print "$time $temp_down:$temp_upper:$temp_out\n";
-		RRDs::update($RRD_DB, "$time:$temp_down:$temp_upper:$temp_out");
+		# print "$time $temps{$DOWN}:$temps{$UPPER}:$temps{$OUT}\n";
+		RRDs::update($RRD_DB, "$time:$temps{$DOWN}:$temps{$UPPER}:$temps{$OUT}");
 		my $err=RRDs::error;
 		if ($err) {
 			{print "problem generating the graph: $err\n";}
 			exit;
 		}
-		undef $temp_upper;
-		undef $temp_down;
-		undef $temp_out;
+		exit
 	}
 }
